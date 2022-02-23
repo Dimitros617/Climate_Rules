856fc7f1cc7f65f2365248e1505c1bcbb144c71d
Dominik Frolík <frolik.dominik@gmail.com>
2022-02-23 11:43:27 +0100

    Uprava formuláře pro certifikaci technologií. fixed #12

--- resources/views/technologi-certificate-verification.blade.php
+++ resources/views/technologi-certificate-verification.blade.php
@@ -3,13 +3,13 @@
     <div id="technology-certificate-verification" admin="{{Auth::permition()->admin}}" first_try="{{$my_nation_technology->first_try}}" hidden></div>
     <input hidden id="admin-pay" class="form-check-input m-0 p-0 ms-1 me-3 mt-1 hidden" role="switch" type="checkbox">
 
-    <div class="fw-bold text-black text-center w-100 fs-2 mb-3 pb-1 mt-2" style="border-bottom: 2px solid black">T31 - Název technologie</div>
+    <div class="fw-bold text-black text-center w-100 fs-2 mb-3 pb-1 mt-2" style="border-bottom: 2px solid black">{{$technology->code}} - {{$technology->technology_name}}</div>
 
     <div class="d-grid w-100 mb-4">
         <div class="flex-wrap d-flex w-100">
-            <div class="w-25 d-grid text-end me-3">
+            <div class="w-30 d-grid text-start pe-3">
                 <span class="fw-bold fs-3 text-black">Popis</span>
-                <span>MIT (+MIK)</span>
+                <span class="text-start fs-6">Popište vybranou technologii a základní princip funkčnosti.</span>
             </div>
             <div class="w-70">
                 <textarea @if($my_nation_technology->first_try == 1 && Auth::permition()->admin !=1) disabled @endif id="technology-certificate-description" class="w-100 p-2 rounded-2 shadow-sm" style="min-height: 100px; min-width: 100%; max-width: 100%" maxlength="2000" placeholder="Zde popište technologii, max 2000 znaků." >{{$my_nation_technology->chose_description}}</textarea>
@@ -20,9 +20,13 @@
 
     <div class="d-grid w-100 mb-4">
         <div class="flex-wrap d-flex w-100">
-            <div class="w-25 d-grid text-end me-3">
+            <div class="w-30 d-grid text-start pe-3">
                 <span class="fw-bold fs-3 text-black">Výhody</span>
-                <span>MIT+MUR (+MIK)</span>
+                <span class="text-start fs-6">
+                    Jaké výhody technologie přináší pro planetu a vaši zemi?
+                    <br>
+                    U každé výhody napište zdůvodnění.
+                </span>
             </div>
             <div class="w-70">
                 <textarea @if($my_nation_technology->first_try == 1 && Auth::permition()->admin !=1) disabled @endif id="technology-certificate-benefits" class="w-100 p-2 rounded-2 shadow-sm" style="min-height: 100px; min-width: 100%; max-width: 100%" maxlength="2000" placeholder="Zde popište výhody technologie, max 2000 znaků.">{{$my_nation_technology->chose_benefits}}</textarea>
@@ -33,9 +37,13 @@
 
     <div class="d-grid w-100 mb-4">
         <div class="flex-wrap d-flex w-100">
-            <div class="w-25 d-grid text-end me-3">
+            <div class="w-30 d-grid text-start pe-3">
                 <span class="fw-bold fs-3 text-black">Nevýhody</span>
-                <span>MIT+MUR (+MIK)</span>
+                <span class="text-start fs-6">
+                    Zhodnoťte kriticky, jaké nevýhody a rizika s sebou nese vybraná technologie?
+                    <br>
+                    U každé nevýhody napište zdůvodnění.
+                </span>
             </div>
             <div class="w-70">
                 <textarea @if($my_nation_technology->first_try == 1 && Auth::permition()->admin !=1) disabled @endif id="technology-certificate-disadvantages" class="w-100 p-2 rounded-2 shadow-sm" style="min-height: 100px; min-width: 100%; max-width: 100%" maxlength="2000" placeholder="Zde popište nevýhody technologie, max 2000 znaků.">{{$my_nation_technology->chose_disadvantages}}</textarea>
@@ -46,9 +54,13 @@
 
     <div class="d-grid w-100 mb-4">
         <div class="flex-wrap d-flex w-100">
-            <div class="w-25 d-grid text-end me-3">
+            <div class="w-30 d-grid text-start pe-3">
                 <span class="fw-bold fs-3 text-black">Business využití</span>
-                <span>MUR</span>
+                <span class="text-start fs-6">
+                    Popište, jaký má technologie potenciál na využití ve firemní sféře.
+                    <br>
+                    Jaké firmy by mohly na technologii vydělávat a jak?
+                </span>
             </div>
             <div class="w-70">
                 <textarea @if($my_nation_technology->first_try == 1 && Auth::permition()->admin !=1) disabled @endif id="technology-certificate-business" class="w-100 p-2 rounded-2 shadow-sm" style="min-height: 100px; min-width: 100%; max-width: 100%" maxlength="2000" placeholder="Zde popište business využití technologe, max 2000 znaků.">{{$my_nation_technology->chose_people}}</textarea>
@@ -59,9 +71,13 @@
 
     <div class="d-grid w-100 mb-4">
         <div class="flex-wrap d-flex w-100">
-            <div class="w-25 d-grid text-end me-3">
-                <span class="fw-bold fs-3 text-black">Zdůvodnění přínosu pro lidi</span>
-                <span>MOO (+MIK)</span>
+            <div class="w-30 d-grid text-start pe-3">
+                <span class="fw-bold fs-3 text-black">Přínos pro lidi</span>
+                <span class="text-start fs-6">
+                    Jak vysvětlíte svým občanům, proč jste investovali právě do této technologie?
+                    <br>
+                    Jaké přínosy má technologie pro lidi a proč?
+                </span>
             </div>
             <div class="w-70">
                 <textarea @if($my_nation_technology->first_try == 1 && Auth::permition()->admin !=1) disabled @endif id="technology-certificate-people" class="w-100 p-2 rounded-2 shadow-sm" style="min-height: 100px; min-width: 100%; max-width: 100%" maxlength="2000" placeholder="Zde zdůvodněte přínos pro lidi, max 2000 znaků.">{{$my_nation_technology->chose_description}}</textarea>
